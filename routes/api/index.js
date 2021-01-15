d98a019f09351ab28bfbd6015a4b7015231d4333
engrcollins <akinbamicollins@gmail.com>
2021-01-15 06:39:14 +0000

    Add README file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,49 +1,13 @@
 const axios = require('axios');
 const express = require('express');
-const { isEmpty } = require('lodash');
 const router = express.Router();
-
-/*router.post('/add', async (req, res) => {
-    if (isEmpty(req.body)) {
-        return res.status(403).json({
-            message: 'Body should not be empty',
-            statusCode: 403
-        });
-    }
-    const { name, msgTitle, msgContent } = req.body;
-
-    const newUser = new User({
-        msgTitle,
-        name,
-        msgContent,
-        date: Date.now()
-    });
-    console.log(newUser)
-    try {
-        await newUser.save();
-        res.json({
-            message: 'Data successfully saved',
-            statusCode: 200,
-            name,
-            msgTitle,
-            msgContent
-        });
-    } catch (error) {
-        console.log('Error: ', error);
-        res.status(500).json({
-            message: 'Internal Server error',
-            statusCode: 500
-        });
-    }
-});
-*/
 
 router.get('/rates', async (req, res) => {
     let homeBase = req.query.base.toUpperCase();
     let currencies = req.query.currency.toUpperCase();
     currencies = currencies.split(',')
-    rates = { };
-    results = { };
+    let rates = { };
+    let results = { };
     let today
     const init = async() => {
         try{
@@ -68,29 +32,4 @@ router.get('/rates', async (req, res) => {
     init();
 });
 
-/*
-router.post('/delete', async (req, res) => {
-    if (isEmpty(req.body)) {
-        return res.status(403).json({
-            message: 'Body should not be empty',
-            statusCode: 403
-        });
-    }
-    const { messageId } = req.body;
-    try {
-        await User.deleteOne({_id:messageId});
-        res.json({
-            message: 'Data successfully deleted',
-            statusCode: 200
-        });
-        console.log('deleted')
-    } catch (error) {
-        console.log('Error: ', error);
-        res.status(500).json({
-            message: 'Internal Server error',
-            statusCode: 500
-        });
-    }
-});
-*/
 module.exports = router;
